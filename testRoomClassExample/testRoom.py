0a8fb83c2b8041f7fbd9319cf2bfa13145a4ab74
Yarahara <69659192+Yarahara@users.noreply.github.com>
2021-06-19 11:37:26 -0700

    Add files via upload

---
+++ testRoomClassExample/testRoom.py
@@ -0,0 +1,18 @@
+#fileName: testRoom.py
+from modRoom import Room
+from datetime import *
+r1 = Room("BR160",50,100)
+print("Room 1:",r1)
+print("Testing getters for Room 1")
+idNum = r1.getRmID();print("ID Number:",idNum)
+rmLength = r1.getRmLength(); print("Length", rmLength)
+rmWidth = r1.getRmWidth(); print("Width",rmWidth)
+rmArea = r1.calcRmArea(); print("Area:",rmArea)
+
+r2 = Room("LI 300",20,40)
+print("\n\nRoom 2: ", r2)
+print("\nTesting setters for Room 2")
+r2.setRmID("Hagen123")
+r2.setRmLength(300); r2.setRmWidth(150)
+print("Changeing r2's into Hagen 123, Length 300, Width 150")
+print(r2)
